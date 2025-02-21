a4e348f16d6bef45460f2623e3478ef52a1a3eae
jjr22 <jjr22@skbl-418-002.ph.ic.ac.uk>
2025-02-21 12:31:51 +0000

    keypad is being recorded properly now

--- KeyPad.s
+++ KeyPad.s
@@ -1,42 +1,57 @@
 #include <xc.inc>
     
-global  Keypad_Setup, Keypad_Transmit_Message
+global  Keypad_Setup, Keypad_read
 
 psect	udata_acs   ; reserve data space in access ram
-Keypad_counter: ds    1	    ; reserve 1 byte for variable UART_counter
-
-psect	uart_code,class=CODE
+Keypad_counter: ds    1	    ; reserve 1 byte for variable Keypad_counter
+delay_count:    ds    1
+col:            ds    1
+row:            ds    1
+combination:    ds    1
+psect	Keypad_code,class=CODE
 Keypad_Setup:
-    bsf	    SPEN	; enable
-    bcf	    SYNC	; synchronous
-    bcf	    BRGH	; slow speed
-    bsf	    TXEN	; enable transmit
-    bcf	    BRG16	; 8-bit generator only
-    movlw   103		; gives 9600 Baud rate (actually 9615)
-    movwf   SPBRG1, A	; set baud rate
-    bsf	    TRISC, PORTC_TX1_POSN, A	; TX1 pin is output on RC6 pin
-					; must set TRISC6 to 1
-    movlb   0xf					
+    movlb   0x0F
+    clrf    TRISH,A
+    clrf    TRISJ,A
+    clrf    TRISF,A
+    clrf    PORTH,A
+    clrf    PORTJ,A
+    clrf    TRISD,A
     bsf     REPU; bit that allows use of pull up resistors
-    clrf    LATE 
-    movlw   0x0F                   
-    movwf   TRISE
+    movlb   0x0
+    clrf    LATE,A
+    clrf    PORTF,A
     return
 
-Keypad_Transmit_Message:	    ; Message stored at FSR2, length stored in W
-    movwf   Keypad_counter, A
-Keypad_Loop_message:
-    movf    POSTINC2, W, A
-    call    Keypad_Transmit_Byte
-    decfsz  Keypad_counter, A
-    bra	    Keypad_Loop_message
+Keypad_read:
+    movlw   0x0F               
+    movwf   TRISE,A
+    call    delay
+    movff   PORTE,combination
+    call    delay
+    movlw   0xF0  
+    movwf   TRISE,A
+    call    delay
+    movf    PORTE,W,A
+    addwf   combination,f,A
+    call Keypad_combine
     return
-
-Keypad_Transmit_Byte:	    ; Transmits byte stored in W
-    btfss   TX1IF	    ; TX1IF is set when TXREG1 is empty
-    bra	    Keypad_Transmit_Byte
-    movwf   TXREG1, A
+    
+Keypad_combine:
+    movff   combination,PORTH
+    call    compare
+    clrf    combination,A
     return
-
+compare:
+    return
+    
+    
+    
+delay:
+    
+    decfsz  delay_count, A	; decrement until zero
+    bra	    delay
+    return
+    
 
 
