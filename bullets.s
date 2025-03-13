a73c58888a8e8adbfce43ef2b31d96786c320631
jackson2206 <jjr22@ic.ac.uk>
2025-03-13 23:16:22 +0000

    discuss with parthener about this page

--- bullets.s
+++ bullets.s
@@ -14,10 +14,15 @@ bullet_Setup:
     lfsr    2,bulletsx
     return
 gen_bullet:  ; takes yadd of player in WREG
-    movwf   POSTINC0,A
+    movwf   POSTINC0,A ;  talk to neil about how to use postinc 
+    movlw   6
+    movwf   POSTINC2,A
     incf    count,f,A
-    lfsr    1,bulletsy
+    lfsr    1,bulletsy ; postdec0 to get earliest
     movf    count,W,A
     return
+move_bullets:; wreg goes to counter
+    movwf   temp,A
+    
 end
 
